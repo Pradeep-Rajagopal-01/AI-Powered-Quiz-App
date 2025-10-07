670fd138e03a49d688e3583bdfbf43a6d30ab5b8
Pradthelegend <pradeep.studies06@gmail.com>
2025-10-06 22:21:28 -0400

    The Quiz game is working as expected as of now evrything is in a single file just need to separate the code and create their own files, aslo have to create a loop for the game and probably will add a points tracker

--- src/main/java/QuizApp.java
+++ src/main/java/QuizApp.java
@@ -1,3 +1,11 @@
 public class QuizApp {
 
+    public static void main(String[] args) {
+        GeminiResponseParser geminiResponseParser =new GeminiResponseParser();
+        try {
+            geminiResponseParser.sendRequest();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
